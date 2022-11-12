4fc81a826da6f01224635051b0ec6031f67b19e4
ENZINHO's PIZZARIA <108340007+EnzoXor@users.noreply.github.com>
2022-11-12 14:42:21 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,233 @@
+const A1 = document.getElementById("A1")
+const A2 = document.getElementById("A2")
+const A3 = document.getElementById("A3")
+const A4 = document.getElementById("A4")
+const A5 = document.getElementById("A5")
+const A6 = document.getElementById("A6")
+const A7 = document.getElementById("A7")
+const A8 = document.getElementById("A8")
+const A9 = document.getElementById("A9")
+const A10 = document.getElementById("A10")
+const B1 = document.getElementById("B1")
+const B2 = document.getElementById("B2")
+const B3 = document.getElementById("B3")
+const B4 = document.getElementById("B4")
+const B5 = document.getElementById("B5")
+const B6 = document.getElementById("B6")
+const B7 = document.getElementById("B7")
+const B8 = document.getElementById("B8")
+const B9 = document.getElementById("B9")
+const B10 = document.getElementById("B10")
+const C1 = document.getElementById("C1")
+const C2 = document.getElementById("C2")
+const C3 = document.getElementById("C3")
+const C4 = document.getElementById("C4")
+const C5 = document.getElementById("C5")
+const C6 = document.getElementById("C6")
+const C7 = document.getElementById("C7")
+const C8 = document.getElementById("C8")
+const C9 = document.getElementById("C9")
+const C10 = document.getElementById("C10")
+const grid = document.querySelector('.grid')
+var bomb1 = ("A1")
+
+var nn = Math.random(1 ,10)
+
+if (bomb1 === "A1")
+{
+    A1.innerHTML = 'bomb'
+}
+
+
+
+
+
+
+
+
+A1.addEventListener("click", ap1)
+function ap1(){
+    A1.removeAttribute("class", "hid")
+    A1.setAttribute("class", "block apa")
+}
+
+A2.addEventListener("click", ap2)
+function ap2(){
+    A2.removeAttribute("class", "hid")
+    A2.setAttribute("class", "block apa")
+}
+
+A3.addEventListener("click", ap3)
+function ap3(){
+    A3.removeAttribute("class", "hid")
+    A3.setAttribute("class", "block apa")
+}
+
+A4.addEventListener("click", ap4)
+function ap4(){
+    A4.removeAttribute("class", "hid")
+    A4.setAttribute("class", "block apa")
+}
+
+A5.addEventListener("click", ap5)
+function ap5(){
+    A5.removeAttribute("class", "hid")
+    A5.setAttribute("class", "block apa")
+}
+
+A6.addEventListener("click", ap6)
+function ap6(){
+    A6.removeAttribute("class", "hid")
+    A6.setAttribute("class", "block apa")
+}
+
+A7.addEventListener("click", ap7)
+function ap7(){
+    A7.removeAttribute("class", "hid")
+    A7.setAttribute("class", "block apa")
+}
+
+A8.addEventListener("click", ap8)
+function ap8(){
+    A8.removeAttribute("class", "hid")
+    A8.setAttribute("class", "block apa")
+}
+
+A9.addEventListener("click", ap9)
+function ap9(){
+    A9.removeAttribute("class", "hid")
+    A9.setAttribute("class", "block apa")
+}
+
+A10.addEventListener("click", ap10)
+function ap10(){
+    A10.removeAttribute("class", "hid")
+    A10.setAttribute("class", "block apa")
+}
+
+B1.addEventListener("click", Bp1)
+function Bp1(){
+    B1.removeAttribute("class", "hid")
+    B1.setAttribute("class", "block apa")
+}
+
+B2.addEventListener("click", Bp2)
+function Bp2(){
+    B2.removeAttribute("class", "hid")
+    B2.setAttribute("class", "block apa")
+}
+
+B3.addEventListener("click", Bp3)
+function Bp3(){
+    B3.removeAttribute("class", "hid")
+    B3.setAttribute("class", "block apa")
+}
+
+B4.addEventListener("click", Bp4)
+function Bp4(){
+    B4.removeAttribute("class", "hid")
+    B4.setAttribute("class", "block apa")
+}
+
+B5.addEventListener("click", Bp5)
+function Bp5(){
+    B5.removeAttribute("class", "hid")
+    B5.setAttribute("class", "block apa")
+}
+
+B6.addEventListener("click", Bp6)
+function Bp6(){
+    B6.removeAttribute("class", "hid")
+    B6.setAttribute("class", "block apa")
+}
+
+B7.addEventListener("click", Bp7)
+function Bp7(){
+    B7.removeAttribute("class", "hid")
+    B7.setAttribute("class", "block apa")
+}
+
+B8.addEventListener("click", Bp8)
+function Bp8(){
+    B8.removeAttribute("class", "hid")
+    B8.setAttribute("class", "block apa")
+}
+
+B9.addEventListener("click", Bp9)
+function Bp9(){
+    B9.removeAttribute("class", "hid")
+    B9.setAttribute("class", "block apa")
+}
+
+B10.addEventListener("click", Bp10)
+function Bp10(){
+    B10.removeAttribute("class", "hid")
+    B10.setAttribute("class", "block apa")
+}
+
+C1.addEventListener("click", Cp1)
+function Cp1(){
+    C1.removeAttribute("class", "hid")
+    C1.setAttribute("class", "block apa")
+}
+
+C2.addEventListener("click", Cp2)
+function Cp2(){
+    C2.removeAttribute("class", "hid")
+    C2.setAttribute("class", "block apa")
+}
+
+C3.addEventListener("click", Cp3)
+function Cp3(){
+    C3.removeAttribute("class", "hid")
+    C3.setAttribute("class", "block apa")
+}
+
+C4.addEventListener("click", Cp4)
+function Cp4(){
+    C4.removeAttribute("class", "hid")
+    C4.setAttribute("class", "block apa")
+}
+
+C5.addEventListener("click", Cp5)
+function Cp5(){
+    C5.removeAttribute("class", "hid")
+    C5.setAttribute("class", "block apa")
+}
+
+C6.addEventListener("click", Cp6)
+function Cp6(){
+    C6.removeAttribute("class", "hid")
+    C6.setAttribute("class", "block apa")
+}
+
+C7.addEventListener("click", Cp7)
+function Cp7(){
+    C7.removeAttribute("class", "hid")
+    C7.setAttribute("class", "block apa")
+}
+
+C8.addEventListener("click", Cp8)
+function Cp8(){
+    C8.removeAttribute("class", "hid")
+    C8.setAttribute("class", "block apa")
+}
+
+C9.addEventListener("click", Cp9)
+function Cp9(){
+    C9.removeAttribute("class", "hid")
+    C9.setAttribute("class", "block apa")
+}
+
+C10.addEventListener("click", Cp10)
+function Cp10(){
+    C10.removeAttribute("class", "hid")
+    C10.setAttribute("class", "block apa")
+}
+
+
+
+
+
+
+
