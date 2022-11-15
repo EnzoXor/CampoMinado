50b573ae757453a39379b0f7c49a395d14feaee3
ENZINHO's PIZZARIA <108340007+EnzoXor@users.noreply.github.com>
2022-11-15 01:49:39 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 function field(cols_cont, rows_cont, mines) {
   var colum = 8
-  var rows = colum
   for (var i = 0; i < colum; i++) {
     const tr = document.createElement('tr')
     const f = document.querySelector('#field')
     f.append(tr)
-    tr.className = i + 1
+
+    tr.setAttribute('id', i + 1)
     for (var j = 0; j < colum; j++) {
       const td = document.createElement('td')
       const l = document.querySelector('tr')
       tr.append(td)
-      td.className = i + 1
+      td.setAttribute('id', j + 1)
       const inp = document.createElement('input')
       td.append(inp)
       inp.setAttribute('type', 'button')
+      inp.setAttribute('onclick', 'see();')
     }
   }
+  const nn1 = Math.floor(Math.random() * colum + 1)
+  const nn2 = Math.floor(Math.random() * colum + 1)
+  console.log(nn1 + ' ' + nn2)
 }
+function see() {
+  console.log('clicou')
+}
+
 field()
